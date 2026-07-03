0cd38f4a74715b8832a99b071f8e78e1c178f6d4
mshivasaitharun <mshivasaitharun@gmail.com>
2026-07-03 22:54:34 +0530

    Updated backend URL

--- interview-ai-main/frontend/index.js
+++ interview-ai-main/frontend/index.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:5000";
+const API_BASE = "https://interview-ai-backend-lepr.onrender.com";
 // or
 // const API_BASE = "http://localhost:5000";
 let mediaRecorder    = null;
